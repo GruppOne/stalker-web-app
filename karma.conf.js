5b9d965cd93a73f0c56e95d0071bcae6c2b680a8
erclu <30255227+erclu@users.noreply.github.com>
2020-03-18 18:46:44 +0100

    ci: try to fix test action

--- karma.conf.js
+++ karma.conf.js
@@ -1,15 +1,15 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+// TODO move karma.conf.js inside src folder
 module.exports = function(config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    // TODO should we add firefox?
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      // TODO should this be here?
-      // require("karma-firefox-launcher"),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma'),
@@ -19,7 +19,12 @@ module.exports = function(config) {
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, './coverage/stalker-web-app'),
-      reports: ['html', 'lcovonly', 'text-summary'],
+      reports: [
+        //
+        'html',
+        'lcovonly',
+        'text-summary',
+      ],
       fixWebpackSourcePaths: true,
       thresholds: {
         statements: 80,
@@ -35,6 +40,12 @@ module.exports = function(config) {
     autoWatch: true,
     // TODO which browsers do we use?
     browsers: ['Chrome'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu'],
+      },
+    },
     singleRun: false,
     restartOnFileChange: true,
   });
