8183b389c4c8fb270cfb4098cfc2909c9bc21c4e
erclu <30255227+erclu@users.noreply.github.com>
2020-03-19 00:41:05 +0100

    ci: finish experimenting with ci

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -10,28 +10,24 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  it('always passes', () => {
-    expect(true).toBe(true);
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
   });
 
-  // it('should create the app', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.componentInstance;
-  //   expect(app).toBeTruthy();
-  // });
+  it(`should have as title 'stalker-web-app'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('stalker-web-app');
+  });
 
-  // it(`should have as title 'stalker-web-app'`, () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.componentInstance;
-  //   expect(app.title).toEqual('stalker-web-app');
-  // });
-
-  // it('should render title', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.nativeElement;
-  //   expect(compiled.querySelector('.content span').textContent).toContain(
-  //     'stalker-web-app app is running!',
-  //   );
-  // });
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'stalker-web-app app is running!',
+    );
+  });
 });
