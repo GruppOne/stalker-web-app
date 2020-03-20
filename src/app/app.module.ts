d5c75ed9f00ed8331dd44cb6822d3b17e3207b16
fabioscettro <fabioscettro@gmail.com>
2020-03-20 23:15:25 +0100

    feat: start design login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {NgModule} from '@angular/core';
-
-import {AppRoutingModule} from './app-routing.module';
+import {CustomMaterialModule} from './core/material.module';
+import {AppRoutingModule} from './core/app.routing.module';
+import {FormsModule} from '@angular/forms';
 import {AppComponent} from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginComponent } from './login/login.component';
+import {LoginComponent} from './login/login.component';
+import {UserComponent} from './user/user.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
-  providers: [],
-  bootstrap: [AppComponent],
+  declarations: [AppComponent, LoginComponent, UserComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    CustomMaterialModule,
+    FormsModule,
+    AppRoutingModule,
+  ],
 })
 export class AppModule {}
