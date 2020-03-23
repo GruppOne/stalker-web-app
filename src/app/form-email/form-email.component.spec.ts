16ad64d997c820fa51e12f903835cd5a4cabd582
Fabio Scettro <fabioscettro@gmail.com>
2020-03-23 16:44:15 +0100

    feat: design login page

---
+++ src/app/form-email/form-email.component.spec.ts
@@ -0,0 +1,24 @@
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {FormEmailComponent} from './form-email.component';
+
+describe('FormEmailComponent', () => {
+  let component: FormEmailComponent;
+  let fixture: ComponentFixture<FormEmailComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [FormEmailComponent],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FormEmailComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/form-email/form-email.component.ts
@@ -0,0 +1,12 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-form-email',
+  templateUrl: 'form-email.component.html',
+  styleUrls: ['form-email.component.scss'],
+})
+export class FormEmailComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
