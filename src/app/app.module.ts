d33327cd52af5dd421449ce00b25695891443894
Fabio Scettro <fabioscettro@gmail.com>
2020-03-23 17:48:37 +0100

    feat: add client side input validation to login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import {CustomMaterialModule} from './core/material.module';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {RouterModule, Routes} from '@angular/router';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {AppComponent} from './app.component';
 import {HomeComponent} from './home/home.component';
 import {LoginComponent} from './login/login.component';
@@ -28,6 +28,7 @@ const routes: Routes = [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(routes),
   ],
   providers: [],
--- src/app/form-email/form-email.component.ts
+++ src/app/form-email/form-email.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-form-email',
@@ -7,6 +8,7 @@ import {Component, OnInit} from '@angular/core';
 })
 export class FormEmailComponent implements OnInit {
   constructor() {}
-
+  // eslint-disable-next-line @typescript-eslint/unbound-method
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
   ngOnInit(): void {}
 }
