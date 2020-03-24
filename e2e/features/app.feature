ce7576079e8ca73f11818ce1a018000aa4c5450e
Alessandro Rizzo <alex.rizzo1998@gmail.com>
2020-03-24 15:41:40 +0100

    test: add cucumber test for the title to be displayed

---
+++ e2e/features/app.feature
@@ -0,0 +1,5 @@
+Feature: Search
+
+  Scenario: Visualize Page title
+    Given I am on the stalker site
+    Then I should see the title to be Stalker-web-app
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,10 @@
 import {browser, by, element} from 'protractor';
 
 export class AppPage {
-  navigateTo(): Promise<unknown> {
-    return browser.get(browser.baseUrl) as Promise<unknown>;
+  public navigateTo() {
+    return browser.get('/');
   }
-
-  getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+  getTitleText() {
+    return element(by.css('app-root .content span')).getText();
   }
 }
