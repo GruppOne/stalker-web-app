a978e5e5d0e0b8db2027775502e7c52619ea7d14
Luca Ercole <30255227+erclu@users.noreply.github.com>
2020-03-27 18:51:11 +0000

    chore: use a different commitizen adapter

---
+++ .cz-config.js
@@ -0,0 +1,88 @@
+const emptyScope = {
+  name: '               empty scope',
+  value: '',
+};
+
+const depsScope = {
+  name: 'deps:          a generic dependency',
+  value: 'deps',
+};
+
+module.exports = {
+  types: [
+    {
+      value: 'feat',
+      name: 'feat:       A new feature',
+    },
+    {
+      value: 'fix',
+      name: 'fix:        A bug fix',
+    },
+    {
+      value: 'docs',
+      name: 'docs:       Documentation only changes',
+    },
+    {
+      value: 'style',
+      name: 'style:      Changes that do not affect the meaning of the code(white-space, formatting, etc)',
+    },
+    {
+      value: 'refactor',
+      name: 'refactor:   A code change that neither fixes a bug nor adds a feature',
+    },
+    {
+      value: 'perf',
+      name: 'perf:       A code change that improves performance',
+    },
+    {
+      value: 'test',
+      name: 'test:       Adding missing tests or correcting existing tests',
+    },
+    {
+      value: 'build',
+      name: 'build:      Changes that affect the build system or external dependencies',
+    },
+    {
+      value: 'chore',
+      name: "chore:      Other changes that don't modify src or test files",
+    },
+    {
+      value: 'revert',
+      name: 'revert:     Revert a previous commit',
+    },
+  ],
+
+  scopes: [
+    // this comments forces prettier to keep the array on multiple lines
+    emptyScope,
+    // TODO add relevant scopes here:
+  ],
+
+  // it needs to match the value for field type. Eg.: 'fix'
+  scopeOverrides: {
+    build: [
+      // this comments forces prettier to keep the array on multiple lines
+      emptyScope,
+      depsScope,
+      'docker',
+      'ng',
+      'npm',
+    ],
+    ci: [
+      // this comments forces prettier to keep the array on multiple lines
+      emptyScope,
+      'actions',
+    ],
+    chore: [
+      // this comments forces prettier to keep the array on multiple lines
+      emptyScope,
+      'vscode',
+    ],
+  },
+
+  allowBreakingChanges: ['feat', 'fix'],
+  allowCustomScopes: false,
+  askForBreakingChangeFirst: true,
+  skipQuestions: [],
+  subjectLimit: 72,
+};
