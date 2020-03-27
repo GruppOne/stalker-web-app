9bfdc6711024b28030812e2802b547f4e25ec4be
Alessandro Rizzo <alex.rizzo1998@gmail.com>
2020-03-27 18:04:31 +0100

    feat: add class UserData and improve class User

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,11 +1,19 @@
+import {UserData} from './user-data';
 export class User {
   private id: number;
   private mail: string;
   private password: string;
-  constructor(email = 'default', password = 'default') {
-    this.id = -1;
+  private userData: UserData;
+  constructor(
+    email = 'default',
+    password = 'default',
+    id = -1,
+    userData = new UserData(),
+  ) {
+    this.id = id;
     this.mail = email;
     this.password = password;
+    this.userData = userData;
   }
   getId(): number {
     return this.id;
@@ -15,5 +23,20 @@ export class User {
   }
   getPassword(): string {
     return this.password;
+  }
+  getUserData(): UserData {
+    return this.userData;
+  }
+  setId(id: number): void {
+    this.id = id;
+  }
+  setEmail(mail: string): void {
+    this.mail = mail;
+  }
+  setPassword(password: string): void {
+    this.password = password;
+  }
+  setUserData(userData: UserData): void {
+    this.userData = userData;
   }
 }
