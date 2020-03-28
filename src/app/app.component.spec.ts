fa4401ec2b411eb6e95617bd38f97320ea4f8106
Alessandro Rizzo <alex.rizzo1998@gmail.com>
2020-03-28 15:10:39 +0100

    test: fix unit test for angular components and services

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,13 +21,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('stalker-web-app');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain(
-      'stalker-web-app is running!',
-    );
-  });
 });
