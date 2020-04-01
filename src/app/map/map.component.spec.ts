8aea53e0f4f1086897fd4524abf6c999d12f8f58
Fabio Scettro <fabioscettro@gmail.com>
2020-04-01 16:40:58 +0200

    chore: resolve errors after rebase

--- src/app/map/map.component.spec.ts
+++ src/app/map/map.component.spec.ts
@@ -1,6 +1,6 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { MapComponent } from './map.component';
+import {MapComponent} from './map.component';
 
 describe('MapComponent', () => {
   let component: MapComponent;
@@ -8,9 +8,8 @@ describe('MapComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MapComponent ]
-    })
-    .compileComponents();
+      declarations: [MapComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
