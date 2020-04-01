13bbabcac585d70772e5bc5e259e07f25b18e4d7
Alessandro Rizzo <alex.rizzo1998@gmail.com>
2020-04-02 01:36:38 +0200

    fix: fix linting error and add profile folder to components/routes

--- src/app/components/_routes/profile/profile.component.ts
+++ src/app/components/_routes/profile/profile.component.ts
@@ -1,9 +1,9 @@
 import {HttpResponse} from '@angular/common/http';
 import {Component, OnInit} from '@angular/core';
 
-import {User, UserBuilder} from '../models/user';
-import {UserData} from '../models/user-data';
-import {UserService} from '../services/user.service';
+import {User, UserBuilder} from '../../../models/users/user';
+import {UserData} from '../../../models/users/user-data';
+import {UserService} from '../../../services/user.service';
 
 @Component({
   selector: 'app-profile',
