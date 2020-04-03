ac89b900888ca206cfb3fcb05fe8063adf1999bd
Alessandro Rizzo <alex.rizzo1998@gmail.com>
2020-04-03 10:05:41 +0200

    test: fix test for home page component

--- src/app/components/_routes/home/home.component.spec.ts
+++ src/app/components/_routes/home/home.component.spec.ts
@@ -1,4 +1,5 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
 
 import {HomeComponent} from './home.component';
 
@@ -9,6 +10,7 @@ describe('HomeComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [HomeComponent],
+      imports: [RouterTestingModule],
     }).compileComponents();
   }));
 
