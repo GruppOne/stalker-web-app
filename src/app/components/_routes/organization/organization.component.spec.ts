87d6d450681bb7d4c8b0ddb3a98046ba983351e7
RiccardoCestaro <riccardo.cestaro@outlook.it>
2020-04-05 16:24:08 +0200

    test: add organization component unit tests

--- src/app/components/_routes/organization/organization.component.spec.ts
+++ src/app/components/_routes/organization/organization.component.spec.ts
@@ -1,13 +1,19 @@
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpResponse, HttpHeaders} from '@angular/common/http';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {of} from 'rxjs';
 
 import {OrganizationComponent} from './organization.component';
 
 describe('OrganizationComponent', () => {
   let component: OrganizationComponent;
   let fixture: ComponentFixture<OrganizationComponent>;
-
+  const httpClient = jasmine.createSpyObj('HttpClient', [
+    // 'post',
+    'get',
+    // 'put',
+    // 'delete',
+  ]);
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [OrganizationComponent],
@@ -24,5 +30,13 @@ describe('OrganizationComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call httpClient get', () => {
+    const httpGetSpy = httpClient.get.and.returnValue(
+      of(new HttpResponse({body: null, headers: new HttpHeaders(), status: 200})),
+    );
+    httpClient.get();
+    expect(httpGetSpy.calls.any()).toBe(true, 'get called');
   });
 });
