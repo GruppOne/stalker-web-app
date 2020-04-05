83c3f652181dfb2fe1b93e93e1acc2fce72c961f
Fabio Scettro <fabioscettro@gmail.com>
2020-04-05 18:32:22 +0200

    test: add some tests to map and edit-organization

--- src/app/components/_routes/edit-organization/edit-organization.component.spec.ts
+++ src/app/components/_routes/edit-organization/edit-organization.component.spec.ts
@@ -1,7 +1,10 @@
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpResponse, HttpHeaders} from '@angular/common/http';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {FormBuilder} from '@angular/forms';
+import {of} from 'rxjs';
+
+import {OrganizationService} from '../../../services/organization.service';
 
 import {EditOrganizationComponent} from './edit-organization.component';
 
@@ -9,11 +12,21 @@ describe('EditOrganizationComponent', () => {
   let component: EditOrganizationComponent;
   let fixture: ComponentFixture<EditOrganizationComponent>;
 
+  const organizationService = jasmine.createSpyObj('OrganizationService', [
+    'getOrganizationById',
+  ]);
+
+  let organizationSpy;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [EditOrganizationComponent],
       imports: [HttpClientTestingModule],
-      providers: [{provide: HttpClient}, {provide: FormBuilder}],
+      providers: [
+        {provide: HttpClient},
+        {provide: FormBuilder},
+        {provide: OrganizationService, useValue: organizationService},
+      ],
     }).compileComponents();
   }));
 
@@ -25,5 +38,13 @@ describe('EditOrganizationComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call the function ', () => {
+    organizationSpy = organizationService.getOrganizationById.and.returnValue(
+      of(new HttpResponse({body: null, headers: new HttpHeaders(), status: 200})),
+    );
+    component.getOrganizationById(1);
+    expect(organizationSpy.calls.any()).toBe(true, 'function called');
   });
 });
