0f5b4e8e105bba22dd0c75967589653f053c14a3
RiccardoCestaro <riccardo.cestaro@outlook.it>
2020-04-05 15:30:09 +0200

    test: add organization and place service unit tests

--- src/app/services/organization.service.spec.ts
+++ src/app/services/organization.service.spec.ts
@@ -1,21 +1,34 @@
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpResponse, HttpHeaders} from '@angular/common/http';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {TestBed} from '@angular/core/testing';
+import {of} from 'rxjs';
 
 import {OrganizationService} from './organization.service';
 
 describe('OrganizationService', () => {
   let service: OrganizationService;
-
+  const httpClient = jasmine.createSpyObj('HttpClient', [
+    // 'post',
+    'get',
+    // 'put',
+    // 'delete',
+  ]);
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [{provide: HttpClient}],
+      providers: [{provide: HttpClient, useValue: httpClient}],
     });
     service = TestBed.inject(OrganizationService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+  it('should call httpClient get', () => {
+    const httpGetSpy = httpClient.get.and.returnValue(
+      of(new HttpResponse({body: null, headers: new HttpHeaders(), status: 200})),
+    );
+    service.getOrganizationById(1);
+    expect(httpGetSpy.calls.any()).toBe(true, 'get called');
   });
 });
