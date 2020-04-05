465046b37fd0d29264bea2fac84d805d998b961b
Fabio Scettro <fabioscettro@gmail.com>
2020-04-05 18:32:22 +0200

    test: add some tests to map and edit-organization

--- src/app/components/map/map.component.spec.ts
+++ src/app/components/map/map.component.spec.ts
@@ -1,6 +1,9 @@
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpResponse, HttpHeaders} from '@angular/common/http';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {of} from 'rxjs';
+
+import {OrganizationService} from '../../services/organization.service';
 
 import {MapComponent} from './map.component';
 
@@ -8,11 +11,19 @@ describe('MapComponent', () => {
   let component: MapComponent;
   let fixture: ComponentFixture<MapComponent>;
 
+  const organizationService = jasmine.createSpyObj('OrganizationService', [
+    'getOrganizationById',
+  ]);
+
+  let organizationSpy;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [MapComponent],
       imports: [HttpClientTestingModule],
-      providers: [{provide: HttpClient}],
+      providers: [
+        {provide: HttpClient},
+        {provide: OrganizationService, useValue: organizationService},
+      ],
     }).compileComponents();
   }));
 
@@ -24,5 +35,13 @@ describe('MapComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call the function ', () => {
+    organizationSpy = organizationService.getOrganizationById.and.returnValue(
+      of(new HttpResponse({body: null, headers: new HttpHeaders(), status: 200})),
+    );
+    component.getOrganizationById(1);
+    expect(organizationSpy.calls.any()).toBe(true, 'function called');
   });
 });
