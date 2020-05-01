c5d456b20a6d743940ac69650c86a453952e308f
Fabio Scettro <fabioscettro@gmail.com>
2020-05-01 12:52:45 +0200

    feat: add random generated components list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,20 @@ import {LoginService} from './model/services/login.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  componentsTooltip = '';
+  components = [
+    'Fabio Scettro',
+    'Alessandro Rizzo',
+    'Riccardo Agatea',
+    'Luca Ercole',
+    'Riccardo Cestaro',
+    'Alberto Cocco',
+    'Alberto Gobbo',
+    'Tobia Apolloni',
+  ];
+
   constructor(public router: Router, readonly loginService: LoginService) {}
+
   getRoute(): string {
     return this.router.url;
   }
@@ -19,4 +32,25 @@ export class AppComponent {
       this.router.navigate(['/home']);
     }
   }
+
+  generateComponents(): void {
+    // empty string
+    this.componentsTooltip = '';
+
+    // Fisher–Yates algorithm to random reorder components array
+    for (let i = this.components.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.components[i], this.components[j]] = [this.components[j], this.components[i]];
+    }
+
+    // append all components to the string
+    this.components.forEach((element) => {
+      this.componentsTooltip += element + ', ';
+    });
+
+    // delete last comma
+    this.componentsTooltip = this.componentsTooltip.slice(0, -2);
+  }
 }
+
+// Fabio Scettro, Alessandro Rizzo, Riccardo Agatea, Luca Ercole, Riccardo Cestaro, Alberto Cocco, Alberto Gobbo, Tobia Apolloni
