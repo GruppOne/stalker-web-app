1eb4d59b0b721e778103c5dc4a17c87db5def2b8
Fabio Scettro <fabioscettro@gmail.com>
2020-05-02 14:55:40 +0200

    feat: import login service in app.components.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import {Component} from '@angular/core';
 import {Router} from '@angular/router';
 
+import {LoginService} from './model/services/login.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  constructor(public router: Router) {}
+  constructor(public router: Router, readonly loginService: LoginService) {}
   getRoute(): string {
     return this.router.url;
   }
