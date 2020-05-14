91b7a321f10a390ed8f0df4f179e59b39d9b4241
Alessandro Rizzo <alex.rizzo1998@gmail.com>
2020-05-14 17:06:27 +0200

    refactor: add comment to authguard for testing organization/ path with mock

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -10,12 +10,14 @@ import {LoginService} from './model/services/login.service';
 export class AuthGuard implements CanActivate {
   constructor(
     private readonly loginService: LoginService,
-    private readonly router: Router,
     private readonly location: Location,
+    private readonly router: Router,
   ) {}
   canActivate(route: ActivatedRouteSnapshot): boolean {
     if (this.loginService.isLoggedIn()) {
       if (route.url.toString().includes('organization')) {
+        // return true;
+        // ---------------- REPLACE THIS WITH THE LINE ABOVE TO TEST ORGANIZATION/ PATHS
         const actualOrgId = +(route.paramMap.get('id') as string);
         if (actualOrgId) {
           if (this.loginService.checkAuthorization(actualOrgId, route.data.roles)) {
@@ -27,6 +29,7 @@ export class AuthGuard implements CanActivate {
         } else {
           return true;
         }
+        // ----------------------------------------------------------------
       } else {
         if (route.url.toString().includes('user')) {
           if ((route.paramMap.get('id') as string) === this.loginService.getUserId()) {
