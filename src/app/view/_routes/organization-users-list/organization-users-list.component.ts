04eeb6014214917fd7fdaca769fcf43b2340d53e
alexrizzus <alex.rizzo1998@mail.com>
2020-05-27 10:52:35 +0200

    feat: add basic element for the users list feature

---
+++ src/app/view/_routes/organization-users-list/organization-users-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-organization-users-list',
+  templateUrl: './organization-users-list.component.html',
+  styleUrls: ['./organization-users-list.component.scss']
+})
+export class OrganizationUsersListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
