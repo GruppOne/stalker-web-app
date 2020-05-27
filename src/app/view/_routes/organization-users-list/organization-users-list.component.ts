1fe70ee9d05d5d340c2ad28ce5286c930d9dcba0
Fabio Scettro <fabioscettro@gmail.com>
2020-05-27 11:53:21 +0200

    feat: start designing users list

--- src/app/view/_routes/organization-users-list/organization-users-list.component.ts
+++ src/app/view/_routes/organization-users-list/organization-users-list.component.ts
@@ -1,10 +1,44 @@
-import {Component, OnInit} from '@angular/core';
-
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+/**
+ * @title Data table with sorting, pagination, and filtering.
+ */
 @Component({
   selector: 'app-organization-users-list',
-  templateUrl: './organization-users-list.component.html',
-  styleUrls: ['./organization-users-list.component.scss'],
+  styleUrls: ['organization-users-list.component.scss'],
+  templateUrl: 'organization-users-list.component.html',
 })
 export class OrganizationUsersListComponent implements OnInit {
-  ngOnInit(): void {}
+  displayedColumns: string[] = ['name', 'email'];
+  dataSource: MatTableDataSource<{
+    id: number;
+    name: string;
+    email: string;
+  }>;
+
+  @ViewChild(MatPaginator, {static: true})
+  paginator!: MatPaginator;
+  @ViewChild(MatSort, {static: true})
+  sort: MatSort = new MatSort();
+
+  constructor() {
+    // Assign the data to the data source for the table to render
+    this.dataSource = new MatTableDataSource();
+  }
+
+  ngOnInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
