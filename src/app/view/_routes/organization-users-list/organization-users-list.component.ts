d86ea893169273ccb1344efdfc679822fe34e5a4
alexrizzus <alex.rizzo1998@mail.com>
2020-05-27 11:43:02 +0200

    feat: add path and fix module for organization users list

--- src/app/view/_routes/organization-users-list/organization-users-list.component.ts
+++ src/app/view/_routes/organization-users-list/organization-users-list.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-organization-users-list',
   templateUrl: './organization-users-list.component.html',
-  styleUrls: ['./organization-users-list.component.scss']
+  styleUrls: ['./organization-users-list.component.scss'],
 })
 export class OrganizationUsersListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
