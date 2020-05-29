2cfb5ccf09c1dc24ce1a7b9f653d4bb54948bbe7
alexrizzus <alex.rizzo1998@mail.com>
2020-05-29 10:08:39 +0200

    feat: add color and maxConcurrentUsers properties to PlaceData

--- src/app/model/classes/places/place-data.ts
+++ src/app/model/classes/places/place-data.ts
@@ -2,6 +2,8 @@ import {MyLatLng} from './my-lat-lng';
 
 export interface PlaceData {
   name?: string;
+  maxConcurrentUsers: number;
+  readonly color?: string;
   readonly polygon: MyLatLng[];
   readonly placeInfo: PlaceInfo;
 }
@@ -14,10 +16,12 @@ export interface PlaceInfo {
 }
 
 export class PlaceDataBuilder {
+  private color?: string;
   constructor(
     private placeInfo: PlaceInfo,
     private name: string,
     private polyline: MyLatLng[],
+    private maxConcurrentUsers: number,
   ) {}
   addPlaceInfo(placeInfo: PlaceInfo): PlaceDataBuilder {
     this.placeInfo = placeInfo;
@@ -31,11 +35,21 @@ export class PlaceDataBuilder {
     this.polyline = polygon;
     return this;
   }
+  addMaxConcurrentUsers(maxConcurrentUsers: number): PlaceDataBuilder {
+    this.maxConcurrentUsers = maxConcurrentUsers;
+    return this;
+  }
+  addColor(color: string): PlaceDataBuilder {
+    this.color = color;
+    return this;
+  }
   build(): PlaceData {
     return {
       name: this.name,
       polygon: this.polyline,
       placeInfo: this.placeInfo,
+      maxConcurrentUsers: this.maxConcurrentUsers,
+      color: this.color,
     };
   }
 }
