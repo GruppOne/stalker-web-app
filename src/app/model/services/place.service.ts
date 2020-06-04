5ed3615cbcd7856da930331dc0a27afaf8c1b139
Alessandro Rizzo <alex.rizzo1998@mail.com>
2020-06-04 16:56:43 +0200

    feat: finish implementing business login for user report

--- src/app/model/services/place.service.ts
+++ src/app/model/services/place.service.ts
@@ -1,6 +1,10 @@
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpResponse} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
+import {HttpClientService} from './http-client.service';
+import {Place} from '../classes/places/place';
+import {PlaceData} from '../classes/places/place-data';
+import {map} from 'rxjs/operators';
 
 export interface Geocoding {
   display_name: string;
@@ -21,7 +25,10 @@ export interface Geocoding {
 export class PlaceService {
   // private readonly stalkerEndpoint: StalkerEndpoint;
 
-  constructor(private readonly httpClient: HttpClient) {
+  constructor(
+    private readonly httpClient: HttpClient,
+    private readonly httpClientService: HttpClientService,
+  ) {
     // this.stalkerEndpoint = new StalkerEndpoint(httpClient, '/place');
   }
 
@@ -29,5 +36,29 @@ export class PlaceService {
     return this.httpClient.get<Geocoding>(
       `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lng}`,
     );
+  }
+
+  getOrgPlaces(orgId: number): Observable<Place[]> {
+    return this.httpClientService
+      .get<Place[]>(`/organization/${orgId}/places`)
+      .pipe(map((response: HttpResponse<Place[]>) => response.body as Place[]));
+  }
+
+  addOrgPlace(orgId: number, newPlace: PlaceData): Observable<boolean> {
+    return this.httpClientService
+      .post<PlaceData>(`/organization/${orgId}/places`, newPlace)
+      .pipe(map(() => true));
+  }
+
+  updateOrgPlace(orgId: number, updatedPlace: Place): Observable<boolean> {
+    return this.httpClientService
+      .put<Place>(`/organization/${orgId}/place/${updatedPlace.id}`, updatedPlace)
+      .pipe(map(() => true));
+  }
+
+  deleteOrgPlace(orgId: number, placeId: number): Observable<boolean> {
+    return this.httpClientService
+      .delete<Place>(`/organization/${orgId}/place/${placeId}`)
+      .pipe(map(() => true));
   }
 }
