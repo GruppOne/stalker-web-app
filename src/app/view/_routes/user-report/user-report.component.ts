3b12d45e411846a907ff5d825867789f4b1398a5
Fabio Scettro <fabioscettro@gmail.com>
2020-06-04 19:17:53 +0200

    feat: improve user-report

--- src/app/view/_routes/user-report/user-report.component.ts
+++ src/app/view/_routes/user-report/user-report.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {UserService, UserMovement} from 'src/app/model/services/user.service';
-import {User} from 'src/app/model/classes/users/user';
 import {ActivatedRoute} from '@angular/router';
+import {User} from 'src/app/model/classes/users/user';
+import {UserService, UserMovement} from 'src/app/model/services/user.service';
 
 @Component({
   selector: 'app-user-report',
@@ -9,7 +9,15 @@ import {ActivatedRoute} from '@angular/router';
   styleUrls: ['./user-report.component.scss'],
 })
 export class UserReportComponent implements OnInit {
-  user?: User;
+  timelineOptions: string[] = ['Today', 'Yesterday', 'This week', 'Ever'];
+  user?: User = {
+    id: 1,
+    data: {
+      firstName: 'Mario',
+      lastName: 'Rossi',
+      birthDate: '1993/02/20',
+    },
+  };
   places = [
     {
       placeId: 1,
@@ -60,5 +68,19 @@ export class UserReportComponent implements OnInit {
         this.userMovementInfo = response;
         console.log(response);
       });
+  }
+
+  calcolateTime(index: number): string {
+    if (index + 1 < this.userMovementInfo.length) {
+      const end = ((this.userMovementInfo[index + 1].time as unknown) as number) / 1000;
+      const start = ((this.userMovementInfo[index].time as unknown) as number) / 1000;
+      const seconds = end - start;
+      if (seconds < 3600) {
+        return `~ ${Math.round(seconds / 60)} minutes`;
+      } else {
+        return `~ ${Math.round(seconds / 3600)} hours`;
+      }
+    }
+    return '';
   }
 }
