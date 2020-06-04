a790491b0ac88d999e22636000d450b599a91dc5
Fabio Scettro <fabioscettro@gmail.com>
2020-06-04 12:44:54 +0200

    feat: add user movements data

--- src/app/view/_routes/user-report/user-report.component.ts
+++ src/app/view/_routes/user-report/user-report.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {UserMovement} from 'src/app/model/services/user.service';
 
 @Component({
   selector: 'app-user-report',
@@ -6,7 +7,29 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./user-report.component.scss'],
 })
 export class UserReportComponent implements OnInit {
-  constructor() {}
+  userName = 'Fabio Scettro';
+  places = [
+    {
+      placeId: 1,
+      placeName: 'Torre Archimede',
+    },
+    {
+      placeId: 2,
+      placeName: 'Complesso Paolotti',
+    },
+  ];
+
+  userMovementInfo: UserMovement[] = [
+    {time: new Date(1608595200), placeId: 1, enter: true},
+    {time: new Date(1608620400), placeId: 1, enter: false},
+    {time: new Date(1608894000), placeId: 2, enter: true},
+    {time: new Date(1608897600), placeId: 2, enter: false},
+  ];
 
   ngOnInit(): void {}
+
+  getPlaceName(placeId: number): string {
+    return this.places.find((element) => element.placeId === placeId)
+      ?.placeName as string;
+  }
 }
